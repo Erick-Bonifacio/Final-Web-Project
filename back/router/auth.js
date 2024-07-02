d3236bea1ede7cfa16db8bfb51c24de59d31051a
Adriano Maretti <dri.cmaretti@gmail.com>
2024-07-01 23:52:32 -0300

    gitignore do back

--- back/router/auth.js
+++ back/router/auth.js
@@ -1,3 +1,27 @@
-//rotas de autenticação passam por aqui
-//importante verificar front - criar pasta auth para login 
+//requiring basic tools
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+
+const pathUsers = path.join(__dirname, "..", "db", "users.json");
+const users = JSON.parse(fs.readFileSync(pathUsers, { encoding: "utf-8" }));
+
+require("dotenv").config();
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const user = users.find((u) => u.email === username && u.senha === password);
+  if (user) {
+    const accessToken = jwt.sign(user, process.env.TOKEN);
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401).send("Usuário ou senha inválidos");
+  }
+});
+
 //http://localhost:8080/auth/login
+module.exports = router;
